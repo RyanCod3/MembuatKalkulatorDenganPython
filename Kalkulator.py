2c7aea4f8017b6e165d60249b63c4ef659d9a6a6
RyanCod3 <112873400+RyanCod3@users.noreply.github.com>
2022-09-06 16:41:16 +0700

    Add files via upload

---
+++ Kalkulator.py
@@ -0,0 +1,85 @@
+import tkinter
+from tkinter import*
+
+root = Tk()
+root.title('Kalkulator')
+root.geometry('560x600+60+200')
+root.resizable(False,False)
+root.configure(bg='#17161b')
+
+equation = ''
+
+def show(value):
+	global equation
+	equation+=value
+	label_result.config(text=equation)
+	
+def clear():
+	global equation
+	equation =''
+	label_result.config(text=equation)
+	
+def calculate():
+	global equation
+	result =''
+	if equation !='':
+		try:
+			result= eval(equation)
+		except:
+			result='error'
+			equation =''
+	label_result.config(text=result)
+			
+
+label_result= Label(root,width=50,height=2,text='',font=('arial',7))
+label_result.pack()
+
+#Baris 1
+Button(root,text='C',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#3697f5',command= lambda: clear()).place(x=10,y=100)
+
+Button(root,text='/',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('/')).place(x=150,y=100)
+
+Button(root,text='%',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('%')).place(x=290,y=100)
+
+Button(root,text='*',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('*')).place(x=430,y=100)
+
+#Baris 2
+Button(root,text='7',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('7')).place(x=10,y=200)
+
+Button(root,text='8',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('8')).place(x=150,y=200)
+
+Button(root,text='9',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('9')).place(x=290,y=200)
+
+Button(root,text='-',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('-')).place(x=430,y=200)
+
+
+#Baris 3
+Button(root,text='4',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('4')).place(x=10,y=300)
+
+Button(root,text='5',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('5')).place(x=150,y=300)
+
+Button(root,text='6',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('6')).place(x=290,y=300)
+
+Button(root,text='+',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('+')).place(x=430,y=300)
+
+#Baris 4
+
+Button(root,text='1',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('1')).place(x=10,y=400)
+
+Button(root,text='2',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('2')).place(x=150,y=400)
+
+Button(root,text='3',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('3')).place(x=290,y=400)
+
+Button(root,text='0',width=11,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('0')).place(x=10,y=500)
+
+#Baris 5
+
+Button(root,text='.',width=3,height=1,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#2a2d36',command= lambda: show('.')).place(x=290,y=500)
+
+Button(root,text='=',width=3,height=4,font=('arial',8,'bold'),bd=1,fg='#fff',bg='#fe9037',command= lambda: calculate()).place(x=430,y=400)
+
+
+
+
+
+root.mainloop()
